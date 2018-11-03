37fd66de783f6a996eab866661f4096e9f6bccfa
Div <gh110827@gmail.com>
2018-11-03 10:15:54 +0800

    修改比例为0.7

--- view/home.js
+++ view/home.js
@@ -5,6 +5,7 @@ const logger = require('../utils/logger');
 
 const getTotal8 = require('../model/getTotal8');
 const getTotalData = require('../model/getTotalData');
+const scale = 0.7
 
 const transformTotal = (str) => {
   let amount = [];
@@ -16,14 +17,14 @@ const transformTotal = (str) => {
       const item = newArr[j].split(',')
       let newObj = {}
       if (i === 0) {
-        newObj.amount = parseInt(item[0] * 0.8)
-        newObj.cash = parseInt(item[1] * 0.8)
-        newObj.nonCash = parseInt(item[2] * 0.8)
+        newObj.amount = parseInt(item[0] * scale)
+        newObj.cash = parseInt(item[1] * scale)
+        newObj.nonCash = parseInt(item[2] * scale)
         amount[j] = newObj
       } else {
-        newObj.number = parseInt(item[0] * 0.8)
-        newObj.cash = parseInt(item[1] * 0.8)
-        newObj.nonCash = parseInt(item[2] * 0.8)
+        newObj.number = parseInt(item[0] * scale)
+        newObj.cash = parseInt(item[1] * scale)
+        newObj.nonCash = parseInt(item[2] * scale)
         quantity[j] = newObj
       }
     }
@@ -36,8 +37,8 @@ const transformData = (arr) => {
   let quantity = [];
   let time = [];
   for (let i = 0; i < arr.length; i++) {
-    amount.push(parseInt(arr[i].TotalAmount * 0.8))
-    quantity.push(parseInt(arr[i].TotalCount * 0.8))
+    amount.push(parseInt(arr[i].TotalAmount * scale))
+    quantity.push(parseInt(arr[i].TotalCount * scale))
     const timeNum = Number(arr[i].PTime.replace(/[^0-9]/ig, ''))
     const date = new Date(timeNum)
     time.push(date.getMonth() + 1 + '/' + date.getDate())
