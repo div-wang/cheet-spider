a2b00634e115570550a5f9bad7aebb3af4bd07d7
Div <gh110827@gmail.com>
2019-06-11 13:38:05 +0800

    增加搜索排序字段

--- model/getSearch.js
+++ model/getSearch.js
@@ -8,6 +8,7 @@ const Search = async (ctx, next) => {
   form.append('IndexTime', time || '本周')
   form.append('Type', type || '') // 1,2,3,4,5
   form.append('Page', page || 1)
+  form.append('isAsc', 1)
   const data = await request.post('Seach', { data: form })
     .then(function (res) {
       return res.text();
